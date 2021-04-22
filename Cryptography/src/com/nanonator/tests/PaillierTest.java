3196bc148003819d997b9c960858f9757ab034d8
[Zachary Moroney] <[s3784630@student.rmit.edu.au]>
2021-04-23 03:36:36 +1000

    Added testing for Paillier.

--- Cryptography/src/com/nanonator/tests/PaillierTest.java
+++ Cryptography/src/com/nanonator/tests/PaillierTest.java
@@ -1,5 +1,38 @@
 package com.nanonator.tests;
 
+import com.nanonator.src.Paillier;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PaillierTest {
-    //TODO Testing for Paillier implementation.
+    Paillier paillier = new Paillier(new BigInteger("11"), new BigInteger("13"), new BigInteger("5652"));
+
+    @Test
+    public void getPublicKeys() {
+        BigInteger[] publicKeys = paillier.getPublicKeys();
+        assertEquals(new BigInteger("143"), publicKeys[0]);
+        assertEquals(new BigInteger("5652"), publicKeys[1]);
+    }
+
+    @Test
+    public void getPrivateKeys() {
+        BigInteger[] privateKeys = paillier.getPrivateKeys();
+        assertEquals(new BigInteger("60"), privateKeys[0]);
+        assertEquals(new BigInteger("53"), privateKeys[1]);
+    }
+
+    @Test
+    public void encrypt() {
+        BigInteger m = new BigInteger("100"), r = new BigInteger("29");
+        assertEquals(new BigInteger("1366"), paillier.encrypt(m, r));
+    }
+
+    @Test
+    public void decrpyt() {
+        BigInteger c = new BigInteger("1366");
+        assertEquals(new BigInteger("100"), paillier.decrypt(c));
+    }
 }
