639362175a2a372f769b58561b1e9f747aac545c
[Zachary Moroney] <[s3784630@student.rmit.edu.au]>
2021-05-18 21:43:41 +1000

    Added JavaDocs. Fixed signature generation.

--- Cryptography/src/com/nanonator/src/ElGamal.java
+++ Cryptography/src/com/nanonator/src/ElGamal.java
@@ -2,11 +2,21 @@ package com.nanonator.src;
 
 import java.math.BigInteger;
 
+/**
+ * ElGamal a partially homomorphic encryption system. It has 3 numbers as the public key and 1 as the private key.
+ *     To create an ElGamal keypair you need 3 input numbers, of which one must be prime.
+ */
 public class ElGamal {
+
     private final BigInteger p, g, y, x;
 
+    /**
+     * Generates an ElGamal keypair using 3 given numbers.
+     * @param p = Random prime number greater than 0.
+     * @param g = Random number greater than 0.
+     * @param x = Random number greater than 0.
+     */
     public ElGamal(BigInteger p, BigInteger g, BigInteger x) {
-        //p, g and x are provided by the user. p is a prime number where g and x are random numbers.
         this.p = p;
         this.g = g;
         this.x = x;
@@ -14,7 +24,13 @@ public class ElGamal {
         this.y = g.modPow(x, p);
     }
 
-    //Constructor to initialise ElGamal using the public and private keys. x can be set to BigInteger.ZERO for encryption only.
+    /**
+     * Initialises an ElGamal keypair using public and private keys. given x value can be BigInteger.ZERO for encryption only.
+     * @param p = p
+     * @param g = g
+     * @param y = y
+     * @param x = x
+     */
     public ElGamal(BigInteger p, BigInteger g, BigInteger y, BigInteger x) {
         this.p = p;
         this.g = g;
@@ -22,35 +38,53 @@ public class ElGamal {
         this.x = x;
     }
 
-
-    //Returns the public keys as an array. Order is p, g, y.
+    /**
+     * Returns the public key as an array of BigInteger.
+     * @return = {p, g, y}
+     */
     public BigInteger[] getPublicKeys() {
         return new BigInteger[]{p, g, y};
     }
 
-    //Returns the private key, x
+    /**
+     * Returns the private key as a BigInteger.
+     * @return = x
+     */
     public BigInteger getPrivateKey() {
         return x;
     }
 
-    //Takes a plaintext message and returns a pair of cryptotext.
+    /**
+     * Takes a plaintext message and random number and returns ciphertext as an array of BigInteger.
+     * @param m = Message
+     * @param r = Random number greater than 0
+     * @return = {c1, c2}
+     */
     public BigInteger[] encrypt(BigInteger m, BigInteger r) {
         BigInteger k = y.modPow(r, p);
-        BigInteger[] cryptos = new BigInteger[2];
-        cryptos[0] = g.modPow(r, p);
-        cryptos[1] = m.multiply(k).mod(p);
-        return cryptos;
+        BigInteger[] ciphertext = new BigInteger[2];
+        ciphertext[0] = g.modPow(r, p);
+        ciphertext[1] = m.multiply(k).mod(p);
+        return ciphertext;
     }
 
-    //Takes a pair of cryptotext and returns the plaintext.
+    /**
+     * Takes an array of cryptotext and returns the plaintext message as a BigInteger
+     * @param c = {c1, c2}
+     * @return = m
+     */
     public BigInteger decrypt(BigInteger[] c) {
         BigInteger k = c[0].modPow(x, p);
         BigInteger invK = k.modInverse(p);
-
         return invK.multiply(c[1]).mod(p);
     }
 
-    //Takes two pairs of cryptotext and multiplies them. Returns a pair of cryptotext.
+    /**
+     * Takes two pairs of ciphertext and multiplies them. Returns the result as an array of BigInteger
+     * @param C1 = {c11, c12}
+     * @param C2 = {c21, c22}
+     * @return = {c31, c32}
+     */
     public BigInteger[] multiply(BigInteger[] C1, BigInteger[] C2) {
      BigInteger[] c = new BigInteger[2];
      c[0] = C1[0].multiply(C2[0]).mod(p);
@@ -58,21 +92,53 @@ public class ElGamal {
      return c;
     }
 
-    public BigInteger decryptMultiplication(BigInteger[] cryptotext) {
-        BigInteger a1 = cryptotext[1].mod(p);
-        BigInteger a2 = (cryptotext[0].pow(x.intValue())).modInverse(p);
+    /**
+     * Takes a pair of previously multiplied ciphertext and decrypts it. Returns the result as a Biginteger.
+     * @param ciphertext = {c1, c2}
+     * @return = m
+     */
+    public BigInteger decryptMultiplication(BigInteger[] ciphertext) {
+        BigInteger a1 = ciphertext[1].mod(p);
+        BigInteger a2 = (ciphertext[0].pow(x.intValue())).modInverse(p);
         return a1.multiply(a2).mod(p);
     }
 
+    /**
+     * Returns the public and private keys as a string.
+     * @return = keys
+     */
     public String toString() {
         return String.format("P = %s, G = %s, X = %s, Y = %s", p.toString(), g.toString(), x.toString(), y.toString());
     }
 
-    //TODO Finish implementation.
+    /**
+     * Generates a signature for the given message.
+     * @param m = Message
+     * @param k = Nonce
+     * @return
+     */
     public BigInteger[] genSignature(BigInteger m, BigInteger k) {
         BigInteger r;
         r = g.modPow(k, p);
-        BigInteger s;
-        return null;
+        BigInteger t = m.subtract(x.multiply(r));
+        BigInteger s = BigUtil.modinv(k, p.subtract(BigInteger.ONE));
+        s = s.multiply(t);
+        s = s.mod(p.subtract(BigInteger.ONE));
+        return new BigInteger[] {r, s};
+    }
+
+    /**
+     * Verifies the signature s of given message m. Returns true if signature matches.
+     * @param m = Message
+     * @param s = Signature[]{r, s}
+     * @return
+     */
+    public boolean verifySignature(BigInteger m, BigInteger[] s) {
+        BigInteger v = g.modPow(m, p);
+        BigInteger t1 = y.pow(s[0].intValue());
+        BigInteger t2 = s[0].pow(s[1].intValue());
+        BigInteger t3 = t1.multiply(t2);
+        BigInteger w = t3.mod(p);
+        return v.equals(w);
     }
 }
